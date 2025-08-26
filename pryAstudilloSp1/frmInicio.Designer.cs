5363ea51ea607804ce4924da2be1e70a1c5d9cc9
astud <astud@LAPTOP-SL7UUHOB>
2025-08-26 01:12:54 -0300

    Agregar archivos de proyecto.

---
+++ pryAstudilloSp1/frmInicio.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace pryAstudilloSp1
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblBienvenido = new Label();
+            btnIngresar = new Button();
+            lblLogo = new Label();
+            SuspendLayout();
+            // 
+            // lblBienvenido
+            // 
+            lblBienvenido.AutoSize = true;
+            lblBienvenido.BorderStyle = BorderStyle.Fixed3D;
+            lblBienvenido.Font = new Font("SimSun", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblBienvenido.Location = new Point(124, 101);
+            lblBienvenido.Name = "lblBienvenido";
+            lblBienvenido.Size = new Size(254, 23);
+            lblBienvenido.TabIndex = 0;
+            lblBienvenido.Text = "Bienvenido al sistema!";
+            lblBienvenido.Click += label1_Click;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(207, 172);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(75, 23);
+            btnIngresar.TabIndex = 1;
+            btnIngresar.Text = "&Ingresar";
+            btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // lblLogo
+            // 
+            lblLogo.AutoSize = true;
+            lblLogo.Font = new Font("Stencil", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblLogo.ForeColor = Color.CornflowerBlue;
+            lblLogo.Location = new Point(12, 9);
+            lblLogo.Name = "lblLogo";
+            lblLogo.Size = new Size(151, 22);
+            lblLogo.TabIndex = 2;
+            lblLogo.Text = "Sintepart SRL";
+            // 
+            // frmInicio
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(495, 283);
+            Controls.Add(lblLogo);
+            Controls.Add(btnIngresar);
+            Controls.Add(lblBienvenido);
+            Name = "frmInicio";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblBienvenido;
+        private Button btnIngresar;
+        private Label lblLogo;
+    }
+}
---
+++ pryAstudilloSp1/frmLogin.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace pryAstudilloSp1
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            l = new Label();
+            lblMódulo = new Label();
+            btnCancelar = new Button();
+            btnAceptar = new Button();
+            cmbMódulo = new ComboBox();
+            txtContraseña = new TextBox();
+            txtUsuario = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario ";
+            // 
+            // l
+            // 
+            l.AutoSize = true;
+            l.Location = new Point(56, 107);
+            l.Name = "l";
+            l.Size = new Size(67, 15);
+            l.TabIndex = 1;
+            l.Text = "Contraseña";
+            // 
+            // lblMódulo
+            // 
+            lblMódulo.AutoSize = true;
+            lblMódulo.Location = new Point(56, 170);
+            lblMódulo.Name = "lblMódulo";
+            lblMódulo.Size = new Size(49, 15);
+            lblMódulo.TabIndex = 2;
+            lblMódulo.Text = "Módulo";
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(343, 98);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(75, 23);
+            btnCancelar.TabIndex = 3;
+            btnCancelar.Text = "&Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Location = new Point(343, 52);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(75, 23);
+            btnAceptar.TabIndex = 4;
+            btnAceptar.Text = "&Aceptar";
+            btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // cmbMódulo
+            // 
+            cmbMódulo.FormattingEnabled = true;
+            cmbMódulo.Location = new Point(150, 162);
+            cmbMódulo.Name = "cmbMódulo";
+            cmbMódulo.Size = new Size(121, 23);
+            cmbMódulo.TabIndex = 5;
+            // 
+            // txtContraseña
+            // 
+            txtContraseña.Location = new Point(150, 99);
+            txtContraseña.Name = "txtContraseña";
+            txtContraseña.Size = new Size(100, 23);
+            txtContraseña.TabIndex = 6;
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(150, 48);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(100, 23);
+            txtUsuario.TabIndex = 7;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(475, 274);
+            Controls.Add(txtUsuario);
+            Controls.Add(txtContraseña);
+            Controls.Add(cmbMódulo);
+            Controls.Add(btnAceptar);
+            Controls.Add(btnCancelar);
+            Controls.Add(lblMódulo);
+            Controls.Add(l);
+            Controls.Add(label1);
+            Name = "frmLogin";
+            Text = "frmLogin";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label l;
+        private Label lblMódulo;
+        private Button btnCancelar;
+        private Button btnAceptar;
+        private ComboBox cmbMódulo;
+        private TextBox txtContraseña;
+        private TextBox txtUsuario;
+    }
+}
